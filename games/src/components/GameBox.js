3915bc11277b715c4274a4380c942b26b96e7d1d
Thanosky <kthanoss90@gmail.com>
2017-02-17 15:00:53 +0100

    added gamesession component

---
+++ games/src/components/GameBox.js
@@ -0,0 +1,15 @@
+import React, {PureComponent} from 'react'
+
+export class GameBox extends PureComponent {
+
+render() {
+  return (
+    <div>
+      <h1> GameBox</h1>
+    </div>
+  )
+}
+
+}
+
+export default (GameBox)
---
+++ games/src/games/GameSession.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+
+export class GameSession extends Component {
+render() {
+  return(
+    <div>
+      <h2> GameSession</h2>
+    </div>
+  )
+}
+
+}
+
+export default GameSession
