2f07eae249431e188f344659685fabd941ea192c
Thanosky <kthanoss90@gmail.com>
2017-02-19 12:15:49 +0100

    game title form

--- games/src/games/GameLobby.js
+++ games/src/games/GameLobby.js
@@ -1,55 +1,56 @@
 import React, { PureComponent, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import RaisedButton from 'material-ui/RaisedButton'
-// import GameItem from '../components/GameItem'
+import GameItem from './GameItem'
+import createGameAction from '../actions/games/create'
 // import setupGames from '../actions/setup-games'
 
 
 export class GameLobby extends PureComponent {
-  // componentDidMount() {
-  //   this.props.setupGames()
-  // }
-  //
-  // createGame() {
-  //   model.create({})
-  // }
-  //
-  // joinGame(game) {
-  //   this.props.joinGame(game, this.props.currentUser)
-  // }
-  //
-  // renderGameItem(game, index) {
-  //   return (
-  //     <GameItem key={index} game={game} onJoin={this.joinGame.bind(this)} />
-  //   )
-  // }
+  constructor(props) {
+    super()
+
+    const { title } = props
+
+    this.state = {
+      title,
+    }
+  }
+
+  createGame(event) {
+    this.setState({
+      title: this.refs.title.value
+    })
+  }
 
   render() {
     return (
       <div>
         <h1>Game Lobby</h1>
         <div>
+          <input type="text" ref="title" className="title"
+            placeholder="Title of the game" /> <br></br>
           <RaisedButton
             label="New Game"
             primary={true}
-            />
+            onClick={this.createGame.bind(this)} />
         </div>
         <div>
           <ul>
-            {/* { this.props.games.map(this.renderGameItem.bind(this)) } */}
+            {  }
           </ul>
         </div>
       </div>
     )
   }
 }
-//
-// const mapStateToProps = (state) => {
-//   return {
-//     games: state.games,
-//     currentGame: state.currentGame,
-//     currentUser: state.currentUser,
-//   }
-// }
+
+const mapStateToProps = (state) => {
+  return {
+    games: state.games,
+    currentGame: state.currentGame,
+    currentUser: state.currentUser,
+  }
+}
 
 export default (GameLobby)
