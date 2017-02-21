734fc79bcc664e32d14616529cdb1e81eb46c0f9
Thanosky <kthanoss90@gmail.com>
2017-02-21 11:19:09 +0100

    paper madness

--- games/src/games/GameSession.js
+++ games/src/games/GameSession.js
@@ -2,6 +2,7 @@ import React, { PureComponent, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import fetchGames from '../actions/games/fetch'
 import Title from '../components/Title'
+import Paper from 'material-ui/Paper';
 import './GameSession.sass'
 import PaperPic from '../images/paper.png'
 import RockPic from '../images/rock.png'
@@ -13,23 +14,23 @@ export class GameSession extends PureComponent {
   render() {
 
     return(
-      <div className="gamesession">
+      <Paper className="gamesession" zDepth={5}>
         <div className="gamesession-title">
           <Title content="Rock, Paper, Scissors" />
         </div>
-        <div className="game-container">
-          <div className="image-container-1">
+        <Paper className="game-container" zDepth={5}>
+          <Paper className="image-container-1" zDepth={5}>
             <img className="rock" src={ RockPic }></img>
             <img className="paper" src={ PaperPic }></img>
             <img className="scissors" src={ ScissorsPic }></img>
-          </div>
-          <div className="image-container-2">
+          </Paper>
+          <Paper className="image-container-2" zDepth={5}>
             <img className="rock" src={ RockPic }></img>
             <img className="paper" src={ PaperPic }></img>
             <img className="scissors" src={ ScissorsPic }></img>
-          </div>
-        </div>
-      </div>
+          </Paper>
+        </Paper>
+      </Paper>
     )
   }
 }
