a1be5aa328b615f4512af3e28afb92f953bd237f
Thanosky <kthanoss90@gmail.com>
2017-02-21 10:21:57 +0100

    basic gamesession markup

--- games/src/games/GameSession.js
+++ games/src/games/GameSession.js
@@ -2,20 +2,42 @@ import React, { PureComponent, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import fetchGames from '../actions/games/fetch'
 import Title from '../components/Title'
+import './GameSession.sass'
 
 export class GameSession extends PureComponent {
-  static propTypes = {}
 
-  componentDidMount() {
-    this.props.fetchgames()
-  }
 
   render() {
-    const { title } = this.props
 
     return(
-      <div className="game session">
-        <Title content={ title } />
+      <div className="gamesession">
+        <div className="gamesession-title">
+          <Title content="Rock, Paper, Scissors" />
+        </div>
+        <div className="game-container">
+          <div className="image-container-1">
+            <img className="rock">
+
+            </img>
+            <img className="paper">
+
+            </img>
+            <img className="scissors">
+
+            </img>
+          </div>
+          <div className="image-container-2">
+            <img className="rock">
+
+            </img>
+            <img className="paper">
+
+            </img>
+            <img className="scissors">
+
+            </img>
+          </div>
+        </div>
       </div>
     )
   }
